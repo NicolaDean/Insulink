605cd07fdd94e7bbba82bdaba4bc10827d00726b
NicolaDean <nicola.dean@mail.polimi.it>
2022-02-19 18:55:17 +0100

    Added an app example to fetch food based on inputs

--- customComponents/fetchExample.js
+++ customComponents/fetchExample.js
@@ -1,41 +1,67 @@
 import React from 'react';
-import { Text, View,FlatList, StyleSheet } from 'react-native';
+import { Text, View,FlatList,Button,TextInput, StyleSheet } from 'react-native';
 
 import { useState,useEffect } from 'react';
 
 export const FetchComponent = () =>{
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const apiLink = "https://api.edamam.com/api/food-database/v2/parser?";
+  const appId  = "21f2be32";
+  const apiKey = "d0e8b5d4eecb5c925ab79b75c85d0583";
 
-  const getMovies = async () => {
+  const [foodData, setData] = useState([]);
+
+  const [foodSelected,setFood] = useState("banana");
+
+
+  const getData = async () => {
     try {
-     const response = await fetch('https://reactnative.dev/movies.json');
-     const json = await response.json();
-     setData(json.movies);
+      //Generate the Link to query food using API
+     const link = getUrl(foodSelected);
+     //Generating POST request to API
+     const response = fetch(link);
+     //WAITING response from API
+     const json = await (await response).json();
+     //SAVING data into the state variable
+     setData(json.hints.slice(0,10));
+     //-------------------------------------
+  //HANDLING ERRORS
    } catch (error) {
      console.error(error);
    } finally {
      setLoading(false);
    }
   }
-   useEffect(() => {
-    getMovies();
-  }, []);
-  
 
+  const getUrl = (userInput) =>{
+      return apiLink +
+            "app_id=" + appId + "&" +
+            "app_key=" + apiKey + "&" +
+            "ingr=" + userInput;
+  }
+  
     return (
        <View>
-           <Text>Custom fetchComponent</Text>
+           <Button title="Get Data" onPress={() => getData()}/>
+
+           <TextInput placeholder="insert food" onChangeText={setFood}/>
+
            <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.title}, {item.releaseYear}</Text>
+            data={foodData}
+            renderItem={({ item }) => (
+            <Text>{item.food.label},{item.food.category}, {item.food.categoryLabel}</Text>
           )}
         />
        </View> 
     );
     
 }
-
+/*
+<FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+*/
 export default FetchComponent;
