ae7264b9b5e675c6b779d6c2d5c50e875ee2eddb
NicolaDean <nicola.dean@mail.polimi.it>
2022-05-06 10:50:00 +0200

    nothing

--- src/utils/chartDataFormatter.js
+++ src/utils/chartDataFormatter.js
@@ -26,36 +26,24 @@ export const glicemyChartFormatter = (glicemyData) =>{
     // and the when you reach that index it passes to the next in order to have alway a fixed number of labels
 
     const numberOfLabels=7;
-    const lenght=Object.keys(glicemyData).length-1;
-    var len=Math.floor(Object.keys(glicemyData).length/numberOfLabels);
-    console.log('todays number of glycemias '+Object.keys(glicemyData).length)
+    const numRecords =Object.keys(glicemyData).length;
+    const lenght=numRecords-1;
+    var len=Math.floor(numRecords/numberOfLabels);
+    console.log('todays number of glycemias '+numRecords)
 
     var i=0;
     glicemyData.forEach( elem =>{
-        
+
         if(i== 0 || i==lenght || i%len == 0){
             chart_data.labels.push(elem.time.hours);
         }
         
         i++;
-        //chart_data.labels.push(elem.time.hours);
         d.push(elem.value);
     })
-    console.log()
-
-    
 
     chart_data.datasets = [{data: d}];
 
-
-    const res = {   
-        labels: l,
-        datasets: [
-            {
-            data: d
-            }
-        ]
-    }
 
     return chart_data;
 }
