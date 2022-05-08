85ffee2cea7e0fd0fcb7e26ee2e9c1fd243b6d45
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-05-08 20:11:03 +0200

    barcode API implementing

--- src/customComponents/barcodeScanner.js
+++ src/customComponents/barcodeScanner.js
@@ -8,7 +8,7 @@ import {View,
   TouchableOpacity,
   Linking,Dimensions
 } from 'react-native';
-
+import { Food_API } from '../utils/apiQuery';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 import { color } from 'react-native-reanimated';
@@ -25,7 +25,7 @@ export const Scanner=()=> {
    //setFlash(RNCamera.Constants.FlashMode.on)
    //setFlash(RNCamera.Constants.FlashMode.off)
 
-  const onSuccess = (e) =>{
+  const onSuccess = async(e) =>{
       /*
     Linking.openURL(e.data).catch(err =>
       console.error('An error occured', err)
@@ -43,6 +43,9 @@ export const Scanner=()=> {
     setError(true)
    }
  */
+   let varia=e.data
+   const json = (await Food_API.getFoodListBarCode('49000036756'));  
+   console.log(JSON.stringify(json))
   setErrorMSG(e.data)
   setError(true)
 };
